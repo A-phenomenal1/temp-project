d08c71c9703acfc1843d2188d38350c1fcef02a9
A-phenomenal1 <niteshch200053@gmail.com>
2022-07-17 13:11:38 +0530

    react deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cipherschools temp project.</h1>
+      <h5>by Nitesh Kumar</h5>
+    </div>
+  );
+}
+
+export default App;
